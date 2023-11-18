812073221c337328965137b05266cb03afa2a723
Katiane <katiane.soares@fatec.sp.gov.br>
2023-11-17 23:49:53 -0300

    criei a lista de clientes

--- src/componentes/Login.tsx
+++ src/componentes/Login.tsx
@@ -1,7 +1,7 @@
 import React, { Component, ChangeEvent } from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import wbimg from '../assets/logo.png';
-import '../styles.css';
+import styles from '../styles.module.css';
 
 interface State {
     email: string;
@@ -36,62 +36,39 @@ class Login extends Component<RouteComponentProps, State> {
     }
 
     handleLogin = (event: React.FormEvent) => {
-        event.preventDefault();
-        this.props.history.push('/clientes');
-    }
-
-    // handleLogin = async (event: React.FormEvent) => {
-    //     event.preventDefault();
-    //     const { email, password } = this.state;
-    //     try {
-    //         const isAuthenticated = await this.authenticate(email, password);
-    //         if (isAuthenticated) {
-    //             this.props.history.push('/clientes');
-    //         } else {
-    //             this.setState({ error: 'Falha na autenticação' });
-    //         }
-    //     } catch (error) {
-    //         this.setState({ error: 'Ocorreu um erro durante a autenticação' });
-    //     }
-    // }
-
-    // authenticate(email: string, password: string): Promise<boolean> {
-
-    //     return Promise.resolve(email === 'usuario@exemplo.com' && password === 'senha');
-    // }
+    event.preventDefault();
+    this.props.history.push('/clientes');
+}
 
     render() {
         return (
-            <div className="container">
-                <div className="container-login">
-                    <div className="wrap-login">
-                        <form className="login-form" onSubmit={this.handleLogin}>
-                        <span className="login-form-title">
+            <div className={styles.container}>
+                <div className={styles['container-login']}>
+                    <div className={styles['wrap-login']}>
+                        <form className={styles['login-form']} onSubmit={this.handleLogin}>
+                        <span className={styles['login-form-title']}>
                                 <img src={wbimg} alt="logo"/> 
                             </span>                                                         
-                            <div className="wrap-input">
-                                <input className={this.state.email !== "" ? 'has-val input' : 'input'} 
+                            <div className={styles['wrap-input']}>
+                                <input className={this.state.email !== "" ? styles['has-val'] + ' ' + styles.input : styles.input} 
                                 type="email" 
                                 value={this.state.email}
                                 onChange={this.handleEmailChange}
                                 required/>
-                                <span className="focus-input" data-placeholder="Email"></span>
+                                <span className={styles['focus-input']} data-placeholder="Email"></span>
                             </div>
-                            <div className="wrap-input">
-                                <input className={this.state.password !== "" ? 'has-val input' : 'input'} 
+                            <div className={styles['wrap-input']}>
+                                <input className={this.state.password !== "" ? styles['has-val'] + ' ' + styles.input : styles.input} 
                                 type="password" 
                                 value={this.state.password}
                                 onChange={this.handlePasswordChange}
                                 required/>
-                                <span className="focus-input" data-placeholder="Password"></span>                                
+                                <span className={styles['focus-input']} data-placeholder="Password"></span>                                
                             </div>   
-                            <div className="container-login-form-btn">
-                                <button className="login-form-btn"type="submit">Entrar</button>
+                            <div className={styles['container-login-form-btn']}>
+                                <button className={styles['login-form-btn']} type="submit">Entrar</button>
                             </div>
-                            <div className="text-center">
-                                <span className="txt1"> Não possui uma conta? </span>                  
-                                <a className="txt2" href="#"> Criar conta </a> 
-                            </div> 
+                            
                         </form>                         
                     </div>
                 </div>
--- src/componentes/listaCliente.tsx
+++ src/componentes/listaCliente.tsx
@@ -1,16 +1,160 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { Component } from "react";
+import React, { Component } from 'react';
+import { Table, Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styles from '../styles.module.css';
 
 
-export default class ListaCliente extends Component {
+type Cliente = {
+    nome: string;
+    nomeSocial: string;
+    cpf: string;
+    rgs: string[];
+    genero: string;
+    dataCadastro: string;
+    telefones: string[];
+}
+
+type State = {
+    clientes: Cliente[];
+    modalShow: boolean;
+    clienteModal: Cliente | null;
+}
+
+export default class ListaClientes extends Component<{}, State> {
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+        this.state = {
+            clientes: [],
+            modalShow: false,
+            clienteModal: null
+        };
+    }
+
+    componentDidMount() {
+        
+        const clientesFicticios: Cliente[] = [
+            {
+                nome: 'João',
+                nomeSocial: 'Jonh',
+                cpf: '22222222222',
+                rgs: ['222222222'],
+                genero: 'Masculino',
+                dataCadastro: new Date().toISOString(),
+                telefones: ['(11) 111111111']
+            },
+            {
+                nome: 'Maria',
+                nomeSocial: 'Mary',
+                cpf: '33333333333',
+                rgs: ['333333333'],
+                genero: 'Feminino',
+                dataCadastro: new Date().toISOString(),
+                telefones: ['(22) 222222222']
+            },
+            
+            {
+                nome: 'Fabio',
+                nomeSocial: 'Fab',
+                cpf: '34343434343',
+                rgs: ['343434343'],
+                genero: 'Masculino',
+                dataCadastro: new Date().toISOString(),
+                telefones: ['(31) 313131313']
+            },
+
+            {
+                nome: 'Ana',
+                nomeSocial: 'Ana',
+                cpf: '44444444444',
+                rgs: ['444444444'],
+                genero: 'Feminino',
+                dataCadastro: new Date().toISOString(),
+                telefones: ['(44) 444444444']
+            },
+
+            {
+                nome: 'Joaquim',
+                nomeSocial: 'Joaq',
+                cpf: '55555555555',
+                rgs: ['555555555'],
+                genero: 'Masculino',
+                dataCadastro: new Date().toISOString(),
+                telefones: ['(55) 555555555']
+            },
+
+            {
+                nome: 'Pedro',
+                nomeSocial: 'Ped',
+                cpf: '66666666666',
+                rgs: ['666666666'],
+                genero: 'Masculino',
+                dataCadastro: new Date().toISOString(),
+                telefones: ['(66) 666666666']
+            }
+
+            
+        ];
+    
+        this.setState({ clientes: clientesFicticios });
+    }
+
+    handleRowClick = (cliente: Cliente) => {
+        this.setState({
+            modalShow: true,
+            clienteModal: cliente
+        });
+    }
+
+    // componentDidMount() {
+    //     // Substitua 'http://localhost:3000/clientes' pela URL do seu servidor
+    //     fetch('http://localhost:3000/clientes')
+    //         .then(response => response.json())
+    //         .then(clientes => this.setState({ clientes }));
+    // }
+
     render() {
+        const { clientes, modalShow, clienteModal } = this.state;
         return (
-            <div className="collection">
-                <a className="collection-item">Cliente 1</a>
-                <a className="collection-item active">Cliente 2</a>
-                <a className="collection-item">Cliente 3</a>
-                <a className="collection-item">Cliente 4</a>
+            <>
+            <div className={styles['container-login']}>
+            <h1>Lista de Clientes</h1>
+            <div className={styles['wrap-lista']}>
+            <Table striped hover>
+                <thead>
+                    <tr>
+                        <th colSpan={2}>Nome</th>
+                        <th colSpan={2}>Telefone</th>
+                        <th colSpan={1}>Gênero</th>
+                        <th colSpan={1}>Consumo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.clientes.map((cliente, index) => (
+                        <tr key={index} onClick={() => this.handleRowClick(cliente)}>
+                            <td colSpan={2}>{cliente.nome}</td>                            
+                            <td colSpan={2}>{cliente.telefones.join(', ')}</td>                            
+                            <td colSpan={1}>{cliente.genero}</td>
+                            <td colSpan={1}></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
             </div>
-        )
+            <Modal show={this.state.modalShow} onHide={() => this.setState({ modalShow: false })}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{this.state.clienteModal?.nome}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Nome Social: {this.state.clienteModal?.nomeSocial}</p>
+                    <p>CPF: {this.state.clienteModal?.cpf}</p>
+                    <p>RGs: {this.state.clienteModal?.rgs.join(', ')}</p>
+                    <p>Gênero: {this.state.clienteModal?.genero}</p>
+                    <p>Data de Cadastro: {this.state.clienteModal?.dataCadastro ? new Date(this.state.clienteModal.dataCadastro).toLocaleDateString('pt-BR') : ''}</p>
+                    <p>Telefones: {this.state.clienteModal?.telefones.join(', ')}</p>
+                </Modal.Body>
+            </Modal>
+            </div>
+        </>
+        );
     }
 }
